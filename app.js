59c45dba9730037095343efc497af91d1c753dc6
Ashish <theashish752@gmail.com>
2023-12-29 20:27:07 +0530

    created the middleware and added the asynchErrorHandler

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const userRouter = require("./routes/user.route");
-
+require("dotenv").config();
+const connectDB = require("./utils/connectDB");
+const { errorhandler } = require("./middleware/error.middleware");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-  
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //----Routes---
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 
-
-
-
-app.listen(PORT, ()=>console.log(`server is Live at ${PORT}`))
+app.use(errorhandler); // middleware should come after the setting up the routes
+connectDB("mongodb://127.0.0.1:27017/searchapp");
+app.listen(PORT, () => console.log(`server is Live at ${PORT}`));
