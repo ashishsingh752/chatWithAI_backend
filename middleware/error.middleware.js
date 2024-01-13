72d33f221c35baf27413f60b57ed5e0835849e74
Ashish <theashish752@gmail.com>
2024-01-13 20:50:05 +0530

    connected the register UI to the backend

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -2,6 +2,7 @@ const errorhandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
+    success: false,
     message: err.message,
     stack: process.env.NODE_ENV === "development" ? err.stack : {},
   });
--- routes/openAI.route.js
+++ routes/openAI.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { apiReqest } = require("../middleware/apiRequest.middleware");
-const { isAuthorised } = require("../middleware/auth.middleware");
-const { openAIController } = require("../controllers/openAI.controller");
+const { apiReqest } = require("../middleware/apiRequest.middleware.js");
+const { isAuthorised } = require("../middleware/auth.middleware.js");
+const { openAIController } = require("../controllers/openAI.controller.js");
 
 const openAIRouter = express.Router();
 
